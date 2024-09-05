1de6798f413a9d3e8af0ec7070d9de78c7b65d10
Ayaan Khan <83865029+xdayaan@users.noreply.github.com>
2024-09-05 20:00:37 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        String aws_secret_token = "AKIAIMNOJVGFDXXXE4OA";
     }
 }
-This is a test file to check for secrets
