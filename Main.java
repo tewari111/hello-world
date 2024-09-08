739aacf8ea4fd43e67d540e1c97654c97255d116
Ayaan Khan <83865029+xdayaan@users.noreply.github.com>
2024-09-08 10:08:10 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,10 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        String aws_secret_token = "AKIAIMNOJVGFDXXXE4OA";
+        
         String push_to_main = "push to main";
 
 
         String another_one = "aonther one";
+
+        String aws_secret_token = "AKIAIMNOJVGFDXXXE4OA";
     }
 }
